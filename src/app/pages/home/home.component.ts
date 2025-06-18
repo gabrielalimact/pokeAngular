4d16cd28ea10a6ff33bb994c581b0d5f5a8d76c3
Gabriela <leticia.lima@nees.ufal.br>
2025-06-18 12:29:47 -0300

    feat pagina home listando todos os pokemons (scroll infinito)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/core/service/pokemon.service';
-import { Pokemon, PokemonList } from 'src/app/models/pokemon.model';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonThumbnail, IonRow, IonCol, IonCardContent, IonGrid} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonThumbnail,
+    RouterModule, CommonModule, IonRow, IonCol, IonCardContent, IonGrid
+  ],
 })
 export class HomeComponent  implements OnInit {
-  pokemonList: Pokemon[] = [];
-  offset: number = 0;
-  limit: number = 20;
+  pokemons: any[] = [];
+  offset = 0;
+  limit = 20;
 
-  constructor(private pokeService: PokemonService) { }
+  constructor(private pokeService: PokemonService) {}
 
   ngOnInit() {
     this.loadPokemons();
   }
 
   loadPokemons(event?: any) {
-    this.pokeService.getAllPokemons(this.offset, this.limit).subscribe((data) => {
-      this.pokemonList = data.results;
-      console.log(data);
+    this.pokeService.getAllPokemons(this.offset, this.limit).subscribe(res => {
+      const calls = res.results.map((p: any) =>
+        this.pokeService.getPokemonByName(p.name)
+      );
+
+      forkJoin(calls).subscribe(data => {
+        this.pokemons.push(...data);
+        console.log('Pokémons carregados até agora:', this.pokemons);
+        if (event) event.target.complete();
+        this.offset += this.limit;
+      });
     });
   }
-
 }
