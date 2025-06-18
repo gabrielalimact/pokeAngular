f9a9f1971519d6dd1a3213c4beb4268b27f2a2b8
Gabriela <leticia.lima@nees.ufal.br>
2025-06-18 12:45:20 -0300

    feat update do module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { IonicStorageModule } from '@ionic/storage-angular';
-
+import { IonicModule } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
+    CommonModule,
     IonicStorageModule.forRoot(),
     HttpClient,
-  ]
+    IonicModule,
+    RouterModule,
+  ],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
