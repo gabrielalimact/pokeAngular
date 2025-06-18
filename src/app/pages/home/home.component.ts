2907c4cbd0e97b3da1a71aa9e83c6ed93aed59ad
Gabriela <leticia.lima@nees.ufal.br>
2025-06-18 13:47:43 -0300

    fix: importacoes nao usadas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/core/service/pokemon.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonThumbnail, IonRow, IonCol, IonCardContent, IonGrid} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInfiniteScroll, IonInfiniteScrollContent, IonRow, IonCol, IonCardContent, IonGrid} from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { forkJoin } from 'rxjs';
+import { Pokemon, PokemonList } from 'src/app/models/pokemon.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonThumbnail,
-    RouterModule, CommonModule, IonRow, IonCol, IonCardContent, IonGrid
+  imports: [IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    RouterModule,
+    CommonModule,
+    IonRow,
+    IonCol,
+    IonCardContent,
+    IonGrid
   ],
 })
 export class HomeComponent  implements OnInit {
-  pokemons: any[] = [];
+  pokemons: Pokemon[] = [];
   offset = 0;
   limit = 20;
 
@@ -25,16 +36,15 @@ export class HomeComponent  implements OnInit {
   }
 
   loadPokemons(event?: any) {
-    this.pokeService.getAllPokemons(this.offset, this.limit).subscribe(res => {
+    this.pokeService.getAllPokemons(this.offset, this.limit).subscribe((res: PokemonList) => {
       const calls = res.results.map((p: any) =>
         this.pokeService.getPokemonByName(p.name)
       );
 
-      forkJoin(calls).subscribe(data => {
+      forkJoin<Pokemon[]>(calls).subscribe((data) => {
         this.pokemons.push(...data);
-        console.log('Pokémons carregados até agora:', this.pokemons);
-        if (event) event.target.complete();
         this.offset += this.limit;
+        if (event) event.target.complete();
       });
     });
   }
